8010cd478c4f5008ae38f2f52ff9510f3780c29f
niranjan <niranjan05527@gmail.com>
2023-01-06 02:01:04 +0800

    upgrade 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingPageComponent } from './listings-page/listings-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingPageComponent
   ],
   imports: [
     BrowserModule,
