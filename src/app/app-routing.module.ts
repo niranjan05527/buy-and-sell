f1d8c69b499535116169dea7b0924c0c58cdaec4
niranjan <niranjan05527@gmail.com>
2023-01-06 02:28:00 +0800

    upgrade 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingPageComponent } from './listings-page/listings-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listings', component:ListingPageComponent, pathMatch:'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
