98cf34582eeead54cd17bbd7ad81eb6195928892
niranjan <niranjan05527@gmail.com>
2023-01-06 02:14:21 +0800

    upgrade 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingPageComponent } from './listings-page/listings-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDetailsPageComponent } from './listing-details-page/listing-details-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListingPageComponent
+    ListingPageComponent,
+    ContactPageComponent,
+    EditListingPageComponent,
+    ListingDetailsPageComponent,
+    MyListingsPageComponent,
+    NewListingPageComponent
   ],
   imports: [
     BrowserModule,
